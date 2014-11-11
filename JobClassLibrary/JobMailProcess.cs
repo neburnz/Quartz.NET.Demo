adc108024f354a4e6bc2ed275fa18a05f6b5e8fe
Ruben Guzman <neburnz@hotmail.com>
2014-11-11 15:41:28 -0600

    Added IJob encapsulating mail process

---
+++ JobClassLibrary/JobMailProcess.cs
@@ -0,0 +1,20 @@
+﻿using MailClassLibrary;
+using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobClassLibrary
+{
+    public class JobMailProcess : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            MailProcess process = new MailProcess();
+
+            process.DoWork();
+        }
+    }
+}
